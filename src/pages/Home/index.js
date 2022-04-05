ac70822ac50c932201afc78c38feb5a8e404d3b6
aylancOnur <aylanc.onur@gmail.com>
2022-04-05 14:23:10 +0300

    fetching posts

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,35 @@
 import styles from "./home.module.css";
 import { Row, Col, List, Avatar } from "antd";
-
-const data = [
-    {
-      title: "Ant Design Title 1",
-    },
-    {
-      title: "Ant Design Title 2",
-    },
-    {
-      title: "Ant Design Title 3",
-    },
-    {
-      title: "Ant Design Title 4",
-    },
-  ];
+import { useQuery } from "@apollo/client";
+import { GET_ALL_POSTS } from "./queries";
+import Loading from "components/Loading";
 
 const Home = () => {
+  const { loading, error, data } = useQuery(GET_ALL_POSTS);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <div>Error: {error.message} </div>;
+  }
+
+  console.log(data);
+
   return (
     <div className={styles.container}>
       <Row justify="center">
         <Col span={14} className={styles.content}>
           <List
             itemLayout="horizontal"
-            dataSource={data}
+            dataSource={data.posts}
             renderItem={(item) => (
               <List.Item>
                 <List.Item.Meta
-                  avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                  avatar={<Avatar src={item.user.profile_photo} />}
                   title={<a href="https://ant.design">{item.title}</a>}
-                  description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                  description={item.description}
                 />
               </List.Item>
             )}
